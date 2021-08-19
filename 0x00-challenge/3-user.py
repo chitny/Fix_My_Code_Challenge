eea9e5068ed2f95099d429ea00914626c7e0d7cd
Ignacio Chitnisky <elinmortal@gmail.com>
2021-08-19 17:15:06 +0000

    setter password fixed

--- 0x00-challenge/3-user.py
+++ 0x00-challenge/3-user.py
@@ -38,8 +38,6 @@ class User():
         - Hash `pwd` in MD5 before assign to `__password`
         """
         if pwd is None or type(pwd) is not str:
-            self.__password = None
-        else:
             self._password = hashlib.md5(pwd.encode()).hexdigest().lower()
 
     def is_valid_password(self, pwd):
